a374d1b4e8b58a334ff6cdcf8d67cbc84ef53336
Miguel <miguelrondon92@gmail.com>
2022-09-16 22:11:58 -0400

    ingest realtor site into DF

--- scrape_realtor.py
+++ scrape_realtor.py
@@ -1,1 +1,17 @@
-import scrapy
+import pandas as pd
+from scrapy import Selector
+import requests 
+
+#url, html, xpath 
+url = "https://www.realtor.com/research/data/"
+html = requests.get(url).text
+xpath = "//table[@id = 'supsystic-table-12']//td[@data-cell-id = 'F3']/a/@href"
+
+sel = Selector(text = html)
+links = sel.xpath(xpath).extract()
+
+#ingest data into pandas DF 
+print("opening link: ")
+print(links[0])
+realtor_df = pd.read_csv(links[0])
+print(realtor_df)
