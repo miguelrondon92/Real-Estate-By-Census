85904ce00128ee0f22955ff9bf59f4c3f864482f
Miguel <miguelrondon92@gmail.com>
2022-09-16 22:40:52 -0400

    copied scrape script to jupyter notebooks tranform

--- scrape_realtor.py
+++ scrape_realtor.py
@@ -1,16 +1,16 @@
 import pandas as pd
 from scrapy import Selector
-import requests 
+import requests
 
-#url, html, xpath 
+#url, html, xpath
 url = "https://www.realtor.com/research/data/"
 html = requests.get(url).text
 xpath = "//table[@id = 'supsystic-table-12']//td[@data-cell-id = 'F3']/a/@href"
 
-sel = Selector(text = html)
+sel = Selector(text=html)
 links = sel.xpath(xpath).extract()
 
-#ingest data into pandas DF 
+#ingest data into pandas DF
 print("opening link: ")
 print(links[0])
 realtor_df = pd.read_csv(links[0])
