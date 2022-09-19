5f714aa1c52665b416a405dd5aa06365b56c1ffb
Miguel <miguelrondon92@gmail.com>
2022-09-18 20:01:02 -0400

    working api test successful

--- census_api.py
+++ census_api.py
@@ -1,6 +1,18 @@
 import requests
 import cen_api 
 
-r = requests.get(cen_api.census_api_url)
 
-print(r)
+host= "https://api.census.gov/data/"
+year= "2020"
+dataset_acronym= "/dec/pl"
+g= "?get="
+variables= "NAME"
+location= "&for=state:*"
+key_pref= "&key="
+key= cen_api.API_KEY
+
+query_url= f"{host}{year}{dataset_acronym}{g}{variables}{location}{key_pref}{key}"
+
+
+r = requests.get(query_url)
+print(r.text)
