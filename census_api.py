b77d938827480d1f6c6405e733c266c111963f74
Miguel <miguelrondon92@gmail.com>
2022-09-19 12:32:29 -0400

    c

--- census_api.py
+++ census_api.py
@@ -6,13 +6,14 @@ host= "https://api.census.gov/data/"
 year= "2020"
 dataset_acronym= "/dec/pl"
 g= "?get="
-variables= "NAME"
-location= "&for=state:*"
+variables = "NAME,P2_001N,P2_002N,P3_004N"
+location= "&for=county:*"
 key_pref= "&key="
 key= cen_api.API_KEY
 
 query_url= f"{host}{year}{dataset_acronym}{g}{variables}{location}{key_pref}{key}"
 
 
-r = requests.get(query_url)
-print(r.text)
+r = requests.get(query_url).text
+print(r)
+print(len(r))
