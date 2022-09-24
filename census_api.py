e1fd3047ec54240f1c59eec5956f84bae8b08fc4
Miguel <miguelrondon92@gmail.com>
2022-09-23 20:48:08 -0400

    create df

--- census_api.py
+++ census_api.py
@@ -2,7 +2,7 @@ import requests
 import cen_api 
 import pandas as pd 
 
-variables = "NAME,P1_001N,P2_001N,P2_002N,P3_004N,P1_022N"
+variables = "NAME,P2_001N,P2_002N,P3_004N"
 
 def import_census_data(variables):
     host= "https://api.census.gov/data/"
@@ -14,12 +14,12 @@ def import_census_data(variables):
     key= cen_api.API_KEY
     query_url= f"{host}{year}{dataset_acronym}{g}{variables}{location}{key_pref}{key}"
     r = requests.get(query_url).json()
-    return r 
+    return [x[0:len(variables.split(","))] for x in r]
 
 
 def get_vars_list(variables):
     census_vars= requests.get("https://api.census.gov/data/2020/dec/pl/variables.json").json()['variables']
-    vars_labels= [census_vars[var]["label"] for var in variables.split(",")[1:-1]]
+    vars_labels= [census_vars[var]["label"] for var in variables.split(",")[1:]]
     vars_labels= [var.replace("!", "") for var in vars_labels]
     vars_labels= ["NAME"] + vars_labels
     return vars_labels
@@ -28,13 +28,13 @@ def get_vars_list(variables):
 def create_df():
     vars_list = get_vars_list(variables)
     census_data = import_census_data(variables)
-    #census_df = pd.DataFrame(census_data, columns=vars_list)
-    print(census_data)
+    census_df = pd.DataFrame(census_data[1:], columns=vars_list)
+    print(census_df.head(20))
 
 
 
 
-print(create_df())
+create_df()
 
 
 
