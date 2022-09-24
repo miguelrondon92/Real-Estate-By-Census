96bc3ce3b937d02529a9d7304af9369af0d300e3
Miguel <miguelrondon92@gmail.com>
2022-09-23 22:50:40 -0400

    No use for mongoDB

--- Census_vars_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-from pymongo import MongoClient
-
-
-census_vars = requests.get("https://api.census.gov/data/2020/dec/pl/variables.json")
-
-client= MongoClient()
-
-db= client["census"]
