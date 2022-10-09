2d65a470fb15ddfcc972185531699155a051fdb3
Miguel <miguelrondon92@gmail.com>
2022-10-08 21:10:58 -0400

    improved sql pull to csv doc

--- sql2csv.sh
+++ sql2csv.sh
@@ -1,1 +1,5 @@
-sql2csv --db "postgresql:///census_realtor_db" --query "SELECT * FROM county_prices AS cp LEFT JOIN census_demographics as cd ON cp.county = cd.county AND cp.state = cd.state" > analytical_files/realtor_census.csv
+sql2csv --db "postgresql:///census_realtor_db" --query "SELECT *
+FROM county_prices AS cp 
+LEFT JOIN census_demographics as cd 
+ON cp.county_fips = cd.fips 
+" > analytical_files/realtor_census.csv
