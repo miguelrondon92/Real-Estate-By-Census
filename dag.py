de45b9f8bc808bbdb1c3e93e274fbf10f25a97c7
Miguel <miguelrondon92@gmail.com>
2022-12-27 21:12:38 -0500

    assigned task to dag

--- dag.py
+++ dag.py
@@ -1,4 +1,11 @@
-from airflow import DAG
+from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators import PythonOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+
+etl_dag= DAG("realtor_census_dashboard_etl")
+
+part_1 = BashOperator(
+    task_id= "Extract & Transform Data",
+    bash_command= 'jupyter nbconvert --execute tranform_dfs.ipynb'
+)
