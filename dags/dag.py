3564e680916b17181cefe51e5373cbabc0e345a9
Miguel <miguelrondon92@gmail.com>
2022-12-30 09:11:13 -0500

    airflow successfully configured

--- dags/dag.py
+++ dags/dag.py
@@ -3,10 +3,12 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
-etl_dag= DAG("realtor_census_dashboard_etl")
+etl_dag= DAG(dag_id= "realtor_census_dashboard_etl",
+            start_date= datetime(2022, 1 ,1),
+            schedule_interval='@daily')
 
 part_1 = BashOperator(
-    task_id= "Extract & Transform Data",
+    task_id= "Extract_n_Transform_Data",
     bash_command= 'jupyter nbconvert --execute tranform_dfs.ipynb',
     dag=etl_dag
 )
