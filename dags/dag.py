2131cab1ea547cd8cdf46791a2fa82991d2c7039
Miguel <miguelrondon92@gmail.com>
2022-12-30 17:14:15 -0500

    created test_dag

--- dags/dag.py
+++ dags/dag.py
@@ -1,7 +1,7 @@
 from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python import PythonOperator
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
 etl_dag= DAG(dag_id= "realtor_census_dashboard_etl",
             start_date= datetime(2022, 1 ,1),
@@ -11,4 +11,14 @@ part_1 = BashOperator(
     task_id= "Extract_n_Transform_Data",
     bash_command= 'jupyter nbconvert --execute tranform_dfs.ipynb',
     dag=etl_dag
+)
+
+test_dag = DAG(dag_id= "test_dag",
+                start_date=date.today(),
+                schedule_interval='@daily')
+
+test_task = BashOperator(
+    task_id= "Test_Task",
+    bash_command= 'echo "Hello World" > hello.txt',
+    dag=test_dag
 )
